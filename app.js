b2ac45fa060869c02a3eb1c16ac6aa845a01bd6d
Jakub Szatybełko <47736299+JakubSzatybelko@users.noreply.github.com>
2024-04-06 01:38:52 +0000

    serve website via endpoint

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const sqlite3 = require('sqlite3').verbose();
 const port = 3001
+const path = require('path');
 var cors = require('cors')
 
 app.use(cors())
@@ -25,6 +26,21 @@ const objectMap = require('./objects');
 // require('./helpers/objectMaper')(db, objectMap);
 // require('./helpers/db-seeder')(db);
 require('./helpers/crud-generator')(app, db, objectMap);
+
+app.get('/', (req, res) => {
+  res.type('.html');
+  res.sendFile(path.join(__dirname + '/DB-viewer/website/index.html'));
+})
+app.get('src/index.js', (req, res) => {
+  res.type('.js');
+  console.log("sime")
+  res.sendFile(path.join(__dirname + '/DB-viewer/website/src/index.js'));
+})
+app.get('src/style.css', (req, res) => {
+  res.type('.css');
+  res.sendFile(path.join(__dirname + '/DB-viewer/website/src/style.css'));
+})
+
 
 app.use(function (req, res) {
   res.status(404);
